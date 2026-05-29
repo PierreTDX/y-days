7dc6081db6805298a51bb7a61ebef9554630748f
PierreTDX <pierre.tondeux@gmail.com>
2026-05-29 14:40:58 +0200

    test predeploy

--- src/pages/onboarding/OnboardingPage.jsx
+++ src/pages/onboarding/OnboardingPage.jsx
@@ -1,13 +1,13 @@
 
-export default function OnboardingPage(){
+export default function OnboardingPage() {
   return (
-    <div className="glass" style={{padding:24}}>
-      <h1 className="title">Bienvenue dans votre kit IA pédagogique</h1>
+    <div className="glass" style={{ padding: 24 }}>
+      <h1 className="title">Bienvenue dans votre kit IA pédagogique V1</h1>
       <p className="subtitle">
         Apprenez à créer des cours rapidement grâce à l’IA.
       </p>
 
-      <div style={{marginTop:20}}>
+      <div style={{ marginTop: 20 }}>
         <button className="btn">Commencer</button>
       </div>
     </div>
